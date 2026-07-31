3d58759bd4c4aba8673c2de7312d36716dc09807
Hariprasath M <hariprasathprem@gmail.com>
2026-07-31 19:25:01 +0530

    Table Creation With Python And Sql

--- EM.py
+++ EM.py
@@ -1,15 +1,30 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
+load_dotenv()
+
 mydb = mysql.connector.connect( 
-    host = '127.0.0.1',
-    user = 'root',
-    password = 'Hari@080802',
+    host = os.getenv("mysql_host"),
+    user = os.getenv("mysql_user"),
+    password =os.getenv("mysql_password"),
+    database = "Employee_Management"
 )
 cursor = mydb.cursor()
 
-cursor.execute("CREATE DATABASE IF NOT EXISTS Employee_Management")
+table= """CREATE TABLE IF NOT EXISTS Employee_Management (
+              Emp_Id INT AUTO_INCREMENT PRIMARY KEY,
+              User_name vARCHAR(50) NOT NUll,
+              Email varchar(50) Unique,
+              Phone VarChar(50),
+              Deparment varchar(15),
+              salary decimal(10,2),
+              Joining_date DATE );
+"""
+cursor.execute(table)
+mydb.commit()
 
-print("Database Created Sucessfully")
+print("Table Created Sucessfully")
 
 cursor.close()
 
