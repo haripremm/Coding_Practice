d8dbe0ed332d940aa2a0794a71fcaf5efe41819f
Hariprasath M <hariprasathprem@gmail.com>
2026-08-08 19:46:10 +0530

    Main Function Example

--- a.py
+++ a.py
@@ -1,6 +1,16 @@
 def function():
 
-    return f" This is a function"
+    return f" This is a function 1"
 
-print(function())
+
+def function2():
+    return " This is function 2"
+if __name__ =="__main__":
+    a= int(input("Enter the number 1 OR 2:"))
+    if a == 1:
+        print(function())
+    elif a==2:
+        print(function2())
+
+
 
