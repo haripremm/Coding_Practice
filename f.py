1b150e982babee8a8a56aeaa218d7a481c6c5264
Hariprasath M <hariprasathprem@gmail.com>
2026-08-10 19:22:46 +0530

    Read The CSV File Using Python

--- f.py
+++ f.py
@@ -4,7 +4,8 @@ employees =[ [1,"Arun","IT", 50000],
              [2,"Priya","HR", 45000 ],
              [3,"Kumar","Finance",60000]]
 
-with open("Employees.csv","w", newline="") as file:
-    writer = csv.writer(file)
-    writer. writerow(["id","Name", "Department", "Salary"])
-    writer.writerows(employees)
+with open("Employees.csv","r") as file:
+    reader = csv.reader(file)
+
+    for i in reader:
+        print(i)
