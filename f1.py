77949a85e30c3098f5e3005d68fdbfaa882e0414
Hariprasath M <hariprasathprem@gmail.com>
2026-08-11 20:45:34 +0530

    Creadtion and  Add data in Json File

--- f1.py
+++ f1.py
@@ -1,3 +1,4 @@
+#Json file creation
 import json
 
 data ={
@@ -10,3 +11,16 @@ with open("mydata.json","w") as f:
     json.dump(data,f,indent=4)
 
 print("Json File Created")
+
+#Updation IN Json file
+
+with open("mydata.json","r") as f:
+    data = json.load(f)
+
+data["Course"] ="Python"
+data["Skill"] = ["Python", "Sql","Power bi", "Ms Office"]
+
+with open("mydata.json","w") as f:
+    json.dump(data, f, indent=4)
+
+print("data added")
