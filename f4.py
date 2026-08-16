8261123d446800d2346641011f2bd64f208eaebe
Hariprasath M <hariprasathprem@gmail.com>
2026-08-16 19:28:57 +0530

    Error handling

--- f4.py
+++ f4.py
@@ -1,39 +1,12 @@
-balance = 50000
-def deposit():
-    amount = int(input("Enter the amount:"))
-    
-    if amount<0:
-        print("Please Enter The Valid amount:")
-    else:
-        balance = balance + amount
-        print("Your Cash Deposit Sucesssfully")
+try:
+    name = input("Enter the Name: ")
+    born_year = int(input("Enter the year:"))
+    age = 2026 - born_year
+    print("You Are" , name, "and Your Age:", age)
+except TypeError:
+    print("Enter a Valid input!")
+except ValueError:
+    print("Enter a Valid input!")
+except ZeroDivisionError:
+    print("Enter a Valid input!")
 
-def Withdraw():
-    amount = int(input("Enter the Cash amount to Withdraw:"))
-    if amount < 0:
-        print("Please Enter the valid amount")
-    elif amount > balance:
-        print("Your Account not have Enough Balance")
-    else:
-        balance = balance - amount
-        print("Your Balance is: {balance}")
-
-def menu():
-    print("Enter  your Option")
-    print("1. Deposit")
-    print("2. Withdraw")
-    print("3. Balance")
-    print("4. Exit")
-
-    Choice = int(input("Enter Your Choice: From 1 - 4  :"))
-
-    if Choice == "1":
-        deposit()
-    elif Choice== "2":
-        Withdraw()
-
-if __name__ =="__main__":
-    menu()
-
-        
-            
